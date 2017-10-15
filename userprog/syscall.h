62462873a32ec91dcd99797efc1d136fe4a5999e
hyun407407 <32672411+hyun407407@users.noreply.github.com>
2017-10-15 16:54:57 +0900

    Add files via upload

--- userprog/syscall.h
+++ userprog/syscall.h
@@ -10,5 +10,7 @@ int write(int fd, const void *buffer, unsigned size);
 pid_t exec(const char *cmd_line);
 int wait(pid_t pid);
 void exit(int status);
+int fibonacci(int n);
+int sum_of_4_integers(int n1, int n2, int n3, int n4);
 /*************/
 #endif /* userprog/syscall.h */
