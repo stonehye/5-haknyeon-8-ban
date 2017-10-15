fb752c00b69ce19bb27a77976e9abb4b9b8474a3
stonehye <stonehye@gmail.com>
2017-10-15 17:52:45 +0900

    Add files via upload
    /examples 에 다른 파일들은 생략

---
+++ examples/sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syscall.h>
+
+int main(int argc, char *argv[])
+{
+	int r_sum, r_fibonacci;
+	int arg1, arg2, arg3, arg4;
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[2]);
+	arg3 = atoi(argv[3]);
+	arg4 = atoi(argv[4]);
+
+	r_fibonacci = fibonacci(arg1);
+	r_sum = sum_of_4_integers(arg1, arg2, arg3, arg4);
+
+	printf("Input:./%s %d %d %d %d\n", argv[0], arg1, arg2, arg3, arg4);
+	printf("Output: %d %d\n", r_fibonacci, r_sum);
+
+}
